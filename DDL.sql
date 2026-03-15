ab17a359ef5cacc85df31fbacc44a8ed468887c2
Christopher MinhKhang Tran <107511729+christran751@users.noreply.github.com>
2026-03-15 11:53:41 -0700

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -67,7 +67,7 @@ CREATE TABLE Characters_Encounters (
 
 CREATE TABLE Turns (
 	idTurns INT NOT NULL AUTO_INCREMENT,
-    idCharacterEncounter INT NOT NULL,
+    idCharacterEncounter INT NOT NULL,  
 	actionOrderInRound INT NOT NULL,
 	roundNumber INT DEFAULT 1,
 	actionTaken VARCHAR(100) NOT NULL,
@@ -80,8 +80,8 @@ CREATE TABLE StatusEffects (
     idStatusEffect INT NOT NULL AUTO_INCREMENT,
     idTurns INT NOT NULL,
     conditionStatus ENUM('Healthy', 'Buffed', 'Poisoned', 'Stunned', 'Dead') DEFAULT 'Healthy',
-    duration INT DEFAULT 1,
-    effectAmount INT DEFAULT 0,
+    duration INT DEFAULT 1,           
+    effectAmount INT DEFAULT 0,     
     PRIMARY KEY (idStatusEffect),
     FOREIGN KEY (idTurns) REFERENCES Turns(idTurns) ON DELETE CASCADE
 );
@@ -96,7 +96,7 @@ CREATE TABLE HealthChangeLogs (
 );
 
 -- Citation for use of AI Tools:
--- Use to help tweak sample data because before Players, i.e., Aaragorn and Gandalf were attacking each other.
+-- Use to help tweak sample data because before Players, i.e., Aaragorn and Gandalf were attacking each other. 
 -- Date: 3/06/2026
 -- Prompt Used to Generated Code:
 -- Can you update my INSERT schema only to make my data feel more DnD-inspired by adding a few monster NPCs, and make sure players can only attack NPCs, not each other.
@@ -123,35 +123,36 @@ VALUES
 -- CHARACTER_ENCOUNTER
 INSERT INTO Characters_Encounters (idCharacters, idEncounters, initiativeOrder, initiativeRoll)
 VALUES
-(1, 1, 2, 14),
-(2, 1, 3, 16),
-(4, 1, 1, 10),
-(3, 2, 2, 19),
-(5, 2, 1, 15),
-(2, 3, 2, 17),
-(6, 3, 1, 12);
+(1, 1, 2, 14),  
+(2, 1, 3, 16),  
+(4, 1, 1, 10), 
+(3, 2, 2, 19),  
+(5, 2, 1, 15), 
+(2, 3, 2, 17),  
+(6, 3, 1, 12);  
 
 -- TURNS
 INSERT INTO Turns (idCharacterEncounter, actionOrderInRound, roundNumber, actionTaken)
 VALUES
-(3, 1, 1, 'Orc Warrior attacks Gandalf for 8 damage'),
-(1, 2, 1, 'Gandalf casts Fireball on Orc Warrior for 25 damage'),
-(2, 3, 1, 'Aragorn strikes Orc Warrior for 12 damage'),
-(5, 1, 1, 'Balrog attacks Ahri for 18 damage'),
-(4, 2, 1, 'Ahri casts Orb of Deception on Balrog for 10 damage'),
-(6, 1, 1, 'Goblin attacks Aragorn for 6 damage'),
-(7, 2, 1, 'Aragorn strikes Goblin for 14 damage');
+(3, 1, 1, 'Orc Warrior attacks Gandalf'),
+(1, 2, 1, 'Gandalf casts Fireball on Orc Warrior'),
+(2, 3, 1, 'Aragorn strikes Orc Warrior'),
+(5, 1, 1, 'Balrog attacks Ahri'),
+(4, 2, 1, 'Ahri casts Orb of Deception on Balrog'),
+(6, 1, 1, 'Goblin attacks Aragorn'),
+(7, 2, 1, 'Aragorn strikes Goblin');
 
 -- HEALTHCHANGE LOGS
 INSERT INTO HealthChangeLogs (idTurns, hitPointChange, hitPointChangeSource)
 VALUES
-(2, -8, 'Orc Warrior attacks Gandalf for 8 damage'),
-(1, -25, 'Gandalf casts Fireball on Orc Warrior for 25 damage'),
-(1, -12, 'Aragorn strikes Orc Warrior for 12 damage'),
-(5, -18, 'Balrog attacks Ahri for 18 damage'),
-(4, -10, 'Ahri casts Orb of Deception on Balrog for 10 damage'),
-(7, -6, 'Goblin attacks Aragorn for 6 damage'),
-(6, -14, 'Aragorn strikes Goblin for 14 damage');
+(2, -8, 'Orc Warrior attacks Gandalf'),
+(1, -25, 'Gandalf casts Fireball on Orc Warrior'),
+(1, -12, 'Aragorn strikes Orc Warrior'),
+(5, -18, 'Balrog attacks Ahri'),
+(4, -10, 'Ahri casts Orb of Deception on Balrog'),
+(7, -6, 'Goblin attacks Aragorn'),
+(6, -14, 'Aragorn strikes Goblin');
+
 
 -- STATUS EFFECT
 INSERT INTO StatusEffects (idTurns, conditionStatus, duration, effectAmount)
@@ -168,5 +169,7 @@ VALUES
 SET FOREIGN_KEY_CHECKS = 1;
 COMMIT;
 
+END //
+DELIMITER ;
 END //
 DELIMITER ;
