b35b8186ecd65692973a95ddfadf633c2a264be6
Christopher MinhKhang Tran <107511729+christran751@users.noreply.github.com>
2026-03-15 11:50:15 -0700

    Update db-connector.js

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,4 +1,13 @@
-// Get an instance of mysql we can use in the app
+/*
+Citation for the following db-connector.js code:
+Date: 02/09/2026
+Copied from / Adapted from: Starter Code for app.js provided by the course
+Source URL: https://canvas.oregonstate.edu/courses/2031764/pages/exploration-web-application-technology-2?module_item_id=26243419
+Type: Starter code / application
+Author: Oregon State University and Dr. Michael Curry
+*/
+
+
 let mysql = require('mysql2')
 
 // Create a 'connection pool' using the provided credentials
@@ -6,11 +15,10 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit   : 10,
     host              : 'classmysql.engr.oregonstate.edu',
-    user              : 'cs340_user',
-    password          : 'password',
-    database          : 'cs340_user'
+    user              : 'user',
+    password          : 'passowrd',
+    database          : 'user'
 }).promise(); // This makes it so we can use async / await rather than callbacks
 
 // Export it for use in our application
 module.exports = pool;
-
