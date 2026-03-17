ea2ee2000427dcbf582cca2eb2f28117c7c54a94
Christopher MinhKhang Tran <107511729+christran751@users.noreply.github.com>
2026-03-16 18:17:09 -0700

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -75,7 +75,7 @@ DELETE FROM Encounters
 WHERE idEncounters = :ID_of_the_row_clicked;
 
 -- /* -----------------------------------------------------------------------------------
--- 3. Characters_Encounters CRUD
+-- 3. CharactersEncounters CRUD
 -- ---------------------------------------------------------------------------------------*/
 
 -- CREATE ---------------------------------------------
@@ -95,11 +95,11 @@ FROM Encounters
 ORDER BY nameOfLocation;
 
 -- ADD or CREATE
-INSERT INTO Characters_Encounters (idCharacters, idEncounters, initiativeOrder, initiativeRoll)
+INSERT INTO CharactersEncounters (idCharacters, idEncounters, initiativeOrder, initiativeRoll)
 VALUES (:idCharactersInput, :idEncountersInput, :initiativeOrderInput, :initiativeRollInput);
 
 -- READ ---------------------------------------------
-SELECT Characters_Encounters.idCharacterEncounter, Characters.displayName, Encounters.nameOfLocation, initiativeOrder, initiativeRoll,
+SELECT CharactersEncounters.idCharacterEncounter, Characters.displayName, Encounters.nameOfLocation, initiativeOrder, initiativeRoll,
 	GREATEST(
         0,
         LEAST(
@@ -108,29 +108,29 @@ SELECT Characters_Encounters.idCharacterEncounter, Characters.displayName, Encou
         )
       ) AS currentHitPoint,
       COALESCE(Characters.maxHitPoint, 0) AS maxHitPoint
-FROM Characters_Encounters JOIN Characters ON Characters_Encounters.idCharacters = Characters.idCharacters
-JOIN Encounters ON Characters_Encounters.idEncounters = Encounters.idEncounters
-LEFT JOIN Turns ON Characters_Encounters.idCharacterEncounter = Turns.idCharacterEncounter
+FROM CharactersEncounters JOIN Characters ON CharactersEncounters.idCharacters = Characters.idCharacters
+JOIN Encounters ON CharactersEncounters.idEncounters = Encounters.idEncounters
+LEFT JOIN Turns ON CharactersEncounters.idCharacterEncounter = Turns.idCharacterEncounter
 LEFT JOIN HealthChangeLogs ON HealthChangeLogs.idTurns = Turns.idTurns
 GROUP BY
-      Characters_Encounters.idCharacterEncounter,
+      CharactersEncounters.idCharacterEncounter,
       Characters.displayName,
       Encounters.nameOfLocation,
-      Characters_Encounters.initiativeOrder,
-      Characters_Encounters.initiativeRoll,
+      CharactersEncounters.initiativeOrder,
+      CharactersEncounters.initiativeRoll,
       Characters.maxHitPoint
-ORDER BY Characters_Encounters.idEncounters, Characters_Encounters.initiativeOrder;
+ORDER BY CharactersEncounters.idEncounters, CharactersEncounters.initiativeOrder;
 
 -- UPDATE ---------------------------------------------
 -- DROP DOWN FOR UPDATE, i.e, get a single data for Update form of Character_Encounters
-  SELECT Characters_Encounters.idCharacterEncounter,
-  CONCAT(Characters.displayName, ' at ', Encounters.nameOfLocation, ' (ID: ', Characters_Encounters.idCharacterEncounter, ')') AS character_encounter  
-  FROM Characters_Encounters
-  JOIN Characters ON Characters_Encounters.idCharacters = Characters.idCharacters
-  JOIN Encounters ON Characters_Encounters.idEncounters = Encounters.idEncounters
-  ORDER BY Characters_Encounters.idCharacterEncounter;
+  SELECT CharactersEncounters.idCharacterEncounter,
+  CONCAT(Characters.displayName, ' at ', Encounters.nameOfLocation, ' (ID: ', CharactersEncounters.idCharacterEncounter, ')') AS character_encounter  
+  FROM CharactersEncounters
+  JOIN Characters ON CharactersEncounters.idCharacters = Characters.idCharacters
+  JOIN Encounters ON CharactersEncounters.idEncounters = Encounters.idEncounters
+  ORDER BY CharactersEncounters.idCharacterEncounter;
 
-UPDATE Characters_Encounters
+UPDATE CharactersEncounters
 SET idCharacters = :idCharactersInput,
 idEncounters = :idEncountersInput,
 initiativeOrder = :initiativeOrderInput,
@@ -138,7 +138,7 @@ initiativeRoll = :initiativeRollInput
 WHERE idCharacterEncounter = :ID_selected_from_browse_page;
 
 -- DELETE ---------------------------------------------
-DELETE FROM Characters_Encounters
+DELETE FROM CharactersEncounters
 WHERE idCharacterEncounter = :ID_of_the_row_clicked;
 
     
@@ -154,9 +154,9 @@ WHERE idCharacterEncounter = :ID_of_the_row_clicked;
 
 -- DROP DOWN TO ADD A NEW TURN BASED OFF OF CHARACTER_ENCOUNTER BECAUSE TURN NEEDS AN EXISTING CHARACTER_ENCOUNTER
 	SELECT  idCharacterEncounter, CONCAT(Characters.displayName, ' - ', Encounters.nameOfLocation, ' (ID: ', idCharacterEncounter, ')') AS displayLabel
-	FROM Characters_Encounters
-	JOIN Characters ON Characters_Encounters.idCharacters = Characters.idCharacters
-	JOIN Encounters ON Characters_Encounters.idEncounters = Encounters.idEncounters
+	FROM CharactersEncounters
+	JOIN Characters ON CharactersEncounters.idCharacters = Characters.idCharacters
+	JOIN Encounters ON CharactersEncounters.idEncounters = Encounters.idEncounters
 	ORDER BY Characters.displayName;
     
 INSERT INTO Turns (idCharacterEncounter, actionOrderInRound, roundNumber, actionTaken)
@@ -172,9 +172,9 @@ Turns.actionTaken,
 Turns.roundNumber,
 Turns.actionOrderInRound
 FROM Turns
-JOIN Characters_Encounters ON Turns.idCharacterEncounter = Characters_Encounters.idCharacterEncounter
-JOIN Characters ON Characters_Encounters.idCharacters = Characters.idCharacters
-JOIN Encounters ON Characters_Encounters.idEncounters = Encounters.idEncounters
+JOIN CharactersEncounters ON Turns.idCharacterEncounter = CharactersEncounters.idCharacterEncounter
+JOIN Characters ON CharactersEncounters.idCharacters = Characters.idCharacters
+JOIN Encounters ON CharactersEncounters.idEncounters = Encounters.idEncounters
 ORDER BY Turns.roundNumber, Turns.actionOrderInRound;
 
 -- UPDATE -----------------------------------------------------------------------------------
@@ -183,9 +183,9 @@ ORDER BY Turns.roundNumber, Turns.actionOrderInRound;
     
     SELECT  Turns.idTurns, CONCAT(Characters.displayName, '-', Encounters.nameOfLocation, ' (ID: ', idTurns, ')')
 	FROM Turns
-	JOIN Characters_Encounters ON Turns.idCharacterEncounter = Characters_Encounters.idCharacterEncounter
-	JOIN Characters ON Characters_Encounters.idCharacters = Characters.idCharacters
-	JOIN Encounters ON Characters_Encounters.idEncounters = Encounters.idEncounters
+	JOIN CharactersEncounters ON Turns.idCharacterEncounter = CharactersEncounters.idCharacterEncounter
+	JOIN Characters ON CharactersEncounters.idCharacters = Characters.idCharacters
+	JOIN Encounters ON CharactersEncounters.idEncounters = Encounters.idEncounters
 	ORDER BY Turns.idTurns ASC;
 
 UPDATE Turns
@@ -212,9 +212,9 @@ WHERE idTurns = :idTurns_of_the_row_clicked;
 -- DROP DOWN AT ADD A NEW HEALTH LOG ENTRY BECAUSE WE CANNOT ADD A NEW ENTRY WITHOUT AN EXISTING TURN
 	SELECT Turns.idTurns, CONCAT(Characters.displayName, ' - ', Encounters.nameOfLocation, ' (Turn ID: ', Turns.idTurns, ')') AS turn_option
 	FROM Turns
-	JOIN Characters_Encounters ON Turns.idCharacterEncounter = Characters_Encounters.idCharacterEncounter
-	JOIN Characters ON Characters_Encounters.idCharacters = Characters.idCharacters
-	JOIN Encounters ON Characters_Encounters.idEncounters = Encounters.idEncounters
+	JOIN CharactersEncounters ON Turns.idCharacterEncounter = CharactersEncounters.idCharacterEncounter
+	JOIN Characters ON CharactersEncounters.idCharacters = Characters.idCharacters
+	JOIN Encounters ON CharactersEncounters.idEncounters = Encounters.idEncounters
 	ORDER BY Turns.idTurns;
 
 INSERT INTO HealthChangeLogs 
@@ -234,9 +234,9 @@ ORDER BY idHealthChangeLogs  ASC;
     CONCAT(Characters.displayName, ' - ', Encounters.nameOfLocation, ' (Log ID: ', HealthChangeLogs.idHealthChangeLogs, ')')
 	FROM HealthChangeLogs
 	JOIN Turns ON HealthChangeLogs.idTurns = Turns.idTurns
-	JOIN Characters_Encounters ON Turns.idCharacterEncounter = Characters_Encounters.idCharacterEncounter
-	JOIN Characters ON Characters_Encounters.idCharacters = Characters.idCharacters
-	JOIN Encounters ON Characters_Encounters.idEncounters = Encounters.idEncounters
+	JOIN CharactersEncounters ON Turns.idCharacterEncounter = CharactersEncounters.idCharacterEncounter
+	JOIN Characters ON CharactersEncounters.idCharacters = Characters.idCharacters
+	JOIN Encounters ON CharactersEncounters.idEncounters = Encounters.idEncounters
 	ORDER BY HealthChangeLogs.idHealthChangeLogs;
 
 UPDATE HealthChangeLogs
@@ -266,9 +266,9 @@ VALUES (:idTurns, :conditionStatus, :duration, :effectAmount);
 SELECT StatusEffects.idStatusEffect, CONCAT(Characters.displayName,' - ', Encounters.nameOfLocation,' - ', Turns.actionTaken), StatusEffects.conditionStatus, StatusEffects.duration, StatusEffects.effectAmount
 FROM StatusEffects 
 JOIN Turns ON StatusEffects.idTurns = Turns.idTurns
-JOIN Characters_Encounters ON Turns.idCharacterEncounter = Characters_Encounters.idCharacterEncounter
-JOIN Characters ON Characters_Encounters.idCharacters = Characters.idCharacters
-JOIN Encounters ON Characters_Encounters.idEncounters = Encounters.idEncounters
+JOIN CharactersEncounters ON Turns.idCharacterEncounter = CharactersEncounters.idCharacterEncounter
+JOIN Characters ON CharactersEncounters.idCharacters = Characters.idCharacters
+JOIN Encounters ON CharactersEncounters.idEncounters = Encounters.idEncounters
 ORDER BY StatusEffects.idStatusEffect;
 
 -- UPDATE ----------------------------------------------------------------------------------- 
@@ -277,9 +277,9 @@ ORDER BY StatusEffects.idStatusEffect;
 	SELECT StatusEffects.idStatusEffect, CONCAT(Characters.displayName, ' - ', Encounters.nameOfLocation, ' (Status Effect ID: ', StatusEffects.idStatusEffect, ')')
 	FROM StatusEffects
 	JOIN Turns ON StatusEffects.idTurns = Turns.idTurns
-	JOIN Characters_Encounters ON Turns.idCharacterEncounter = Characters_Encounters.idCharacterEncounter
-	JOIN Characters ON Characters_Encounters.idCharacters = Characters.idCharacters
-	JOIN Encounters ON Characters_Encounters.idEncounters = Encounters.idEncounters
+	JOIN CharactersEncounters ON Turns.idCharacterEncounter = CharactersEncounters.idCharacterEncounter
+	JOIN Characters ON CharactersEncounters.idCharacters = Characters.idCharacters
+	JOIN Encounters ON CharactersEncounters.idEncounters = Encounters.idEncounters
 	ORDER BY StatusEffects.idStatusEffect;
 		
 UPDATE StatusEffects
@@ -292,4 +292,3 @@ WHERE idStatusEffect = :StatusEffects_ID_from_update_form;
 -- DELETE -----------------------------------------------------------------------------------
 DELETE FROM StatusEffects
 WHERE idStatusEffect = :StatusEffects_ID_selected_from_browse_page;
-
